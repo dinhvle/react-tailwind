da93336daaaf87c078330e9ec6b09f9353cf1f77
Dinh Le <dinh.v.le@gmail.com>
2021-04-10 12:14:28 -0500

    :tada: setup react app with tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img className="mx-auto h-12 w-auto" src={logo} alt="tailwindcss logo" />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              React with Tailwindcss
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
